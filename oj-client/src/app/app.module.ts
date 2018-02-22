cbee2602c0da7a7a5fb4418c60ba9cd45a050553
Yingjin Chen <yingjincs@gmail.com>
2018-02-21 20:14:23 -0500

    add data service and first component

--- oj-client/src/app/app.module.ts
+++ oj-client/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DataService } from './services/data.service';
 
 import { AppComponent } from './app.component';
+import { ProblemListComponent } from './components/problem-list/problem-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemListComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
